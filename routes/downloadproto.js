1a1c75fcf70160725eab57cc3f0553b1f1dac9f2
chaoulgs <chaoulgs@vcu.edu>
2017-02-24 00:31:48 -0500

    fiddling with route

--- routes/downloadproto.js
+++ routes/downloadproto.js
@@ -5,8 +5,8 @@ var router = express.Router();
 //Routes
 
 //homepage
-router.get('/download', function(req, res){
-	res.download('./public/downloads/AmazingTestGrader103.zip');
+router.get('/downloadproto', function(req, res){
+	res.download('./public/downloads/grader-v2-prototype.zip');
 });
 //return router
 module.exports = router;
